25bed009a5adb6a5ee19e0534a7f7a7b1915b7c6
79141 <dimas_tol_1998@mail.eu>
2021-12-05 21:02:41 +0300

    Калькулятор

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -11,12 +11,26 @@ public class Converter {
         return rsl;
     }
 
+    public static float euroToDollar(float value) {
+        float rsl = value / 0.8f;
+        return rsl;
+    }
+
+    public static float dollarToEuro(float value) {
+        float rsl = value / 1.13f;
+        return rsl;
+    }
+
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         float dollars = Converter.rubleToDollar(1500);
+        float dollar = Converter.euroToDollar(5);
+        float euros = Converter.dollarToEuro(10);
 
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("1500 rubles are " + dollars + " dollars.");
+        System.out.println("5 euro are " + dollar + " dollars.");
+        System.out.println("10 dollars are " + euros + " euro.");
 
     }
 }
--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,11 +1,13 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
     public static void main(String[] args) {
         int size = 10;
-        int result = size - 5;
         size = 100;
-        System.out.println(result);
+        System.out.println(size);
+        size = size - 5;
         System.out.println(size);
     }
+
 }
